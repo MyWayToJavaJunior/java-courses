74b128b0393b6c0298f28c5ea486503806af57a3
MyWayToJavaJunior <hyperjava@mail.ru>
2015-06-25 18:01:27 +0600

    update

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,14 @@
 public class Calculate {
-
+	/**
+	* Выполняется сложение двух входящих аргументов указанных при старте программы, с выведением их в консоль
+	* @author DK
+	* @version 1.0.1
+	* @date 25.06.2015
+	* @param arg Массив входящих аргументов
+	* @param first Первый входящий аргумент
+	* @param second Второй входящий аргумент
+	* @param summ Результат сложения входящий аргументов first и second
+	*/
 	public static void main(String[] arg) {
 	
 		System.out.println("Calculated .....");
@@ -7,6 +16,7 @@ public class Calculate {
 		int second = Integer.valueOf(arg[1]);
 		int summ = first + second;
 		System.out.println("Summ of " +first +" and " +second +" is " +summ);
+		
 	}
 
 }
